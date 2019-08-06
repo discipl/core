bab86cc7bfbd4c30c862209f63394024d2dc20a9
Pim Otte <potte@quintor.nl>
2019-08-06 10:54:53 +0200

    Adding jsdoc to html conversion

--- src/connector-loader.js
+++ src/connector-loader.js
@@ -1,10 +1,14 @@
 const CONNECTOR_MODULE_PREFIX = '@discipl/core-'
 
 /**
- * loads a connector based on module name using a dynamic import
+ * Loads a connector based on module name using a dynamic import
+ *
+ * @param {string} connectorName
+ * @returns {object} The corresponding connector
  */
 const loadConnector = async (connectorName) => {
   let module = await import(CONNECTOR_MODULE_PREFIX + connectorName)
+
   return module.default
 }
 
